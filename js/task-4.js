270a09cac58f2033520d11280c994b23d0fba2f9
pixel <pixel.koval@gmail.com>
2025-02-15 00:23:21 +0200

    rdy 5 tasks

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const emailField = form.elements.email;
+    const passwordField = form.elements.password;
+
+    const emailValue = emailField.value.trim();
+    const passwordValue = passwordField.value.trim();
+
+    if (emailValue === "" || passwordValue === "") {
+        alert("All form fields must be filled in");
+        return;
+    }
+    
+    const formData = {
+        email: emailValue,
+        password: passwordValue
+    };
+        
+    console.log(formData);
+    form.reset();
+}
 
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,9 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("#controls input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
